881ba856561fe034e5c791562d2199ce79aa80d5
ankursrv <122548800+ankursrv@users.noreply.github.com>
2023-10-03 14:31:55 +0530

    Add files via upload

---
+++ Ecomerce_site/js/file.js
@@ -0,0 +1,30 @@
+// buynow btn 
+$(document).ready (function(){
+    $("#buynow-btn a").mouseover(function(){
+        $("#buynow-btn a").css({"background":"black","transition":"ease 0.5s"});
+        });
+        
+    $("#buynow-btn a").mouseleave(function(){
+        $("#buynow-btn a").css({"background": "" ,"transition":"ease 0.5s",});
+        });
+
+
+});
+
+
+//wow
+
+// wow = new WOW(
+//     {
+//     boxClass:     'wow',      // default
+//     animateClass: 'animated', // default
+//     offset:       0,          // default
+//     mobile:       true,       // default
+//     live:         true        // default
+//   }
+//   )
+//   wow.init();
+
+    
+
+    
